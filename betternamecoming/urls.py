eb968761a3710b15874cd0f62a4e1e968ca2bdd0
Joel Cross <joel@kazbak.co.uk>
2015-04-28 15:24:18 +0300

    Add home page

--- betternamecoming/urls.py
+++ betternamecoming/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'betternamecoming.views.home', name='home'),
+    url(r'^$', TemplateView.as_view(template_name="public.html"), name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
