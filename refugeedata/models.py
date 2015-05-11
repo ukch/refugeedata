882cdbc2a0dff756945b0dda3914188f27eb0cb4
Joel Cross <joel@kazbak.co.uk>
2015-05-11 14:52:57 +0300

    Add translation strings

--- refugeedata/models.py
+++ refugeedata/models.py
@@ -13,11 +13,13 @@ class RegistrationNumber(models.Model):
     It is not necessarily assigned to a person.
     """
 
-    id = UUIDField(auto=True, primary_key=True)
-    number = models.PositiveSmallIntegerField()
-    active = models.BooleanField(default=False)
+    id = UUIDField(auto=True, primary_key=True, verbose_name=_("ID"))
+    number = models.PositiveSmallIntegerField(verbose_name=_("number"))
+    active = models.BooleanField(default=False, verbose_name=_("active"))
 
     class Meta:
+        verbose_name = _("Registration Number")
+        verbose_name_plural = _("Registration Numbers")
         ordering = ("number", )
 
     def __unicode__(self):
@@ -29,50 +31,66 @@ class RegistrationCardBatch(models.Model):
     This can be auto-generated alongside registration numbers, or linked to
     existing registration numbers.
     """
-    registration_numbers = models.ManyToManyField(RegistrationNumber)
-    pdf = models.FileField(blank=True, null=True)
+    registration_numbers = models.ManyToManyField(
+        RegistrationNumber,
+        verbose_name=_("Registration Numbers"))
+    pdf = models.FileField(blank=True, null=True, verbose_name=_("PDF File"))
 
     def __unicode__(self):
-        return unicode(self.registration_number_format)
+        return unicode(self.registration_number_format())
 
-    @property
     def registration_number_format(self):
         numbers = self.registration_numbers.order_by("number")
         return utils.format_range(numbers)
+    registration_number_format.short_description = _("Number Range")
 
     class Meta:
         ordering = ("id", )
-        verbose_name_plural = _("Registration card batches")
+        verbose_name = _("Registration Card Batch")
+        verbose_name_plural = _("Registration Card Batches")
 
 
 class Language(models.Model):
     iso_code = LanguageField(verbose_name=_("Base Language"))
-    description = models.CharField(max_length=255)
-    example_text = models.TextField(max_length=255)
+    description = models.CharField(max_length=255,
+                                   verbose_name=_("Description"))
+    example_text = models.TextField(max_length=255,
+                                    verbose_name=_("Example Text"))
 
     def __unicode__(self):
         return u"{}: {}".format(self.iso_code, self.description)
 
+    class Meta:
+        ordering = ("iso_code", )
+        verbose_name = _("Language")
+        verbose_name_plural = _("Languages")
+
 
 class Person(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, verbose_name=_("Name"))
     preferred_lang = models.ForeignKey(Language,
                                        verbose_name=_("Preferred language"))
-    needs = models.TextField(blank=True, null=True)
-    email = models.EmailField(blank=True, null=True)
+    needs = models.TextField(blank=True, null=True, verbose_name=_("Needs"))
+    email = models.EmailField(blank=True, null=True, verbose_name=_("Email"))
     # TODO incredibly basic phone number validation
-    phone = models.CharField(max_length=20, blank=True, null=True)
+    phone = models.CharField(max_length=20, blank=True, null=True,
+                             verbose_name=_("Phone Number"))
     preferred_contact = models.CharField(max_length=1, choices=[
         ("P", _("Phone")),
         ("E", _("Email")),
-    ], default="E")
-    story = models.TextField(blank=True, null=True)
-    active = models.BooleanField(default=True)
+    ], default="E", verbose_name=_("Preferred Contact"))
+    story = models.TextField(blank=True, null=True, verbose_name=_("Story"))
+    active = models.BooleanField(default=True, verbose_name=_("Active"))
 
     # Populated by mobile (although fallback is available)
     registration_card = models.OneToOneField(
         RegistrationNumber,
         related_name="person",
         limit_choices_to={"person": None, "active": False},
+        verbose_name=_("Registration Card"),
     )
-    photo = models.ImageField(blank=True, null=True)
+    photo = models.ImageField(blank=True, null=True, verbose_name=_("Photo"))
+
+    class Meta:
+        verbose_name = _("Person")
+        verbose_name_plural = _("People")
