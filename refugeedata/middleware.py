5fc1e23286a14f4e577b78cd163fa41cb454bf9f
Joel Cross <joel@kazbak.co.uk>
2015-05-19 13:02:49 +0300

    Fix site redirection
    Add some middleware tests

--- refugeedata/middleware.py
+++ refugeedata/middleware.py
@@ -1,17 +1,29 @@
-from django.conf import settings
+from django.apps import apps
 from django.http import HttpResponseRedirect
 
-from django.contrib.sites.middleware import CurrentSiteMiddleware
 from django.contrib.sites.models import Site
+from django.contrib.sites.requests import RequestSite
 
 
-class EnforceCurrentSiteMiddleware(CurrentSiteMiddleware):
+def get_current_site(request):
+    """
+    Version of sites.shortcuts.get_current_site that always uses the request
+    """
+    # Imports are inside the function because its point is to avoid importing
+    # the Site models when django.contrib.sites isn't installed.
+    if apps.is_installed('django.contrib.sites'):
+        return Site.objects._get_site_by_request(request)
+    else:
+        return RequestSite(request)
+
+
+class EnforceCurrentSiteMiddleware():
     """Make sure the current site is one of those listed in Sites.
     If it is not, redirect to the correct site."""
 
     def process_request(self, request):
         try:
-            super(EnforceCurrentSiteMiddleware, self).process_request(request)
+            request.site = get_current_site(request)
         except Site.DoesNotExist:
             request.site = None
             a_site = Site.objects.all()[0]
