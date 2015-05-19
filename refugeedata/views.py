89284e5a50cd71fe564c0c65a9509c4b1e039f73
Joel Cross <joel@kazbak.co.uk>
2015-05-19 20:05:48 +0300

    Only redirect to admin for admin users

--- refugeedata/views.py
+++ refugeedata/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import redirect, render
 
 
 def home(request):
-    if request.user.is_staff:
+    if request.user.is_superuser:
         return redirect("admin:index")
     if request.user.has_perm("refugeedata.add_person"):
         return redirect("reg:home")
