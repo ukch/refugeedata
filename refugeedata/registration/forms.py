1d0c6510261d8d683499eabfd30bd7a6298430fb
Joel Cross <joel@kazbak.co.uk>
2015-06-22 11:58:31 +0300

    Reformat preferred language selector

--- refugeedata/registration/forms.py
+++ refugeedata/registration/forms.py
@@ -4,7 +4,7 @@ from refugeedata.models import Person
 
 
 def _preferred_language_label_from_instance(instance):
-    return u"{} ({})".format(instance.example_text, instance.description)
+    return u"{}, {}: {}".format(instance.iso_code, instance.description, instance.example_text)
 
 
 class RegistrationForm(forms.ModelForm):
