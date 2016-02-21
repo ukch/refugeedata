edccc1a5209c6f9eb0e37ef86195d367c54105a2
Joel Cross <ukch@users.noreply.github.com>
2016-02-21 13:36:18 +0000

    Update wsgi.py

--- refugeedata/app/wsgi.py
+++ refugeedata/app/wsgi.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 
 import os
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "refugeedata.app.settings")
+
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "refugeedata.app.settings")
 
 application = DjangoWhiteNoise(get_wsgi_application())
