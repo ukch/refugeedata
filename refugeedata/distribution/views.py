8c8f2ac33f14c3f2190121a31f8063b242e9da01
Joel Cross <ukch@users.noreply.github.com>
2016-05-03 16:44:34 +0100

    Send template ID instead of 'to' addresses when >=50 addresses (#4)
    * Pass template ID where # recipients >= 50
    
    * Handle to_template
    
    * Add distribution_id to to_template

--- refugeedata/distribution/views.py
+++ refugeedata/distribution/views.py
@@ -136,10 +136,13 @@ def template_to_mailer(request, distribution, template_id):
             "template": template,
         })
     params = {
-        "to": "; ".join(recipients),
         "body": body.encode("utf-8"),
         "next": request.path,
     }
+    if len(recipients) >= 50:
+        params["to_template"] = "{}:{}".format(template.id, "" if "to_everyone" in request.GET else distribution.id)
+    else:
+        params["to"] = "; ".join(recipients)
     url_name = ("mailings:send_email"
                 if template.type == models.ONE_DIGIT_CODE_EMAIL
                 else "mailings:send_sms")
